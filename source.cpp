7d6dd59cfc9905fc2b213231a9a5074d7c78cc68
MykolaPMP <31858361+MykolaPMP@users.noreply.github.com>
2017-09-21 22:33:48 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -3,6 +3,9 @@
 #include <fstream>
 using namespace std;
 
+void DecimalToBinary(char c, int *word);
+char BinaryToChar(int *word);
+
 
 
 int main()
@@ -13,6 +16,11 @@ int main()
 	BITMAPINFOHEADER bih_l;
 	RGBTRIPLE rgb_l;
 	FILE * f1, *f2;
+
+	char*hide = new char[100];
+	int *word = new int[7];
+
+
 
 	f1 = fopen("n.bmp", "r+b");
 	f2 = fopen("4.bmp", "w+b");
@@ -28,44 +36,97 @@ int main()
 		padding = 4 - (bih_l.biWidth * 3) % 4;
 	}
 
+	cout << "Input your word to code" << endl;
+	cin >> hide;
+
 	for (int i = 0; i< 1; i++)
 	{
 		
-		/*for (int j = 0; j < 10000; j++)
-		{*/
-			while (!feof(f1))
+		while (!feof(f1))
+		{
+			//coding
+			for (int i = 0; i < strlen(hide); i++) 
 			{
-				fread(&rgb_l, sizeof(rgb_l), 1, f1);
-
-				//cout << (byte)rgb_l.rgbtBlue << ' ';
-			/*	if (rgb_l.rgbtGreen < 255)
+				DecimalToBinary(hide[i],word);
+				for (int j = 0; j < 7; j++)
 				{
-					rgb_l.rgbtGreen=rgb_l.rgbtGreen+1;
-				}*/
-				//cout << (int)rgb_l.rgbtBlue << endl;
+					fread(&rgb_l, sizeof(rgb_l), 1, f1);
+					if (word[j] == 1) 
+					{
+						if (rgb_l.rgbtBlue % 2 != 1) 
+						{
+							rgb_l.rgbtBlue++;
+						}
+					}
+					else
+					{
+						if (rgb_l.rgbtBlue % 2 != 0)
+						{
+							rgb_l.rgbtBlue--;
+						}
+					}
+						fwrite(&rgb_l, sizeof(rgb_l), 1, f2);
+					}
+				}
 
+				fread(&rgb_l, sizeof(rgb_l), 1, f1);
 				fwrite(&rgb_l, sizeof(rgb_l), 1, f2);
-
 				if (padding != 0)
 				{
 					fread(&rgb_l, padding, 1, f1);
 					fwrite(&rgb_l, padding, 1, f2);
 				}
-			}
-		/*}*/
-
-		
+			}	
 	}
 	fclose(f1);
 	fclose(f2);
-
-	/*f2 = fopen("4.bmp", "r");
+	f2 = fopen("4.bmp", "r");
 	fread(&bfh_l, sizeof(bfh_l), 1, f2);
 	fread(&bih_l, sizeof(bih_l), 1, f2);
-	fclose(f2);*/
+	//decoding
+	for (int i = 0; i < strlen(hide) * 7; i++)
+	{
+		fread(&rgb_l, sizeof(rgb_l), 1, f1);
+		if (rgb_l.rgbtBlue % 2 == 0)
+		{
+			word[i % 7] = 0;
+		}
+		else 
+		{ 
+			word[i % 7] = 1; 
+		}
+		if (i % 7 == 6) 
+		{
+			cout << BinaryToChar(word);
+		}
+	}
+	cout << endl;
+	fclose(f2);
 
 
-
+	delete[] word; delete[] hide;
 	system("pause");
 	return 0;
+}
+
+
+void DecimalToBinary(char c, int *word)
+{
+	int number = (int)c;
+	int i = 6;
+	while (number != 0)
+	{
+		word[i] = number % 2;
+		number /= 2;
+		i--;
+	}
+}
+char BinaryToChar(int *word)
+{
+	int n = 0;
+	for (int i = 0; i < 7; i++)
+	{
+		n += word[i] * pow(2, 6 - i);
+	}
+	return n;
 }
